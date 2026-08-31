2d445b2675ff5efc19852a05df226f92e1e61cd7
rmzneydev <neider9725@gmail.com>
2026-08-31 10:43:39 -0500

    feat: register inventory management and supplyrequest  routes

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -5,6 +5,8 @@ import authRoutes from "./auth.routes";
 import clinicRoutes from "./clinic.routes";
 import warehouseRoutes from "./warehouse.routes";
 import medicineRoutes from "./medicine.routes";
+import inventoryRoutes from "./inventory.routes";
+import supplyRequestRoutes from "./supplyRequest.routes";
 
 const router = Router();
 
@@ -14,6 +16,8 @@ router.use("/auth", authRoutes);
 router.use("/clinics", clinicRoutes);
 router.use("/warehouses", warehouseRoutes);
 router.use("/medicines", medicineRoutes);
+router.use("/inventory", inventoryRoutes);
+router.use("/requests", supplyRequestRoutes);
 
 
 export default router;
